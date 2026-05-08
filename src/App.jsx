469429963f8370a90ddee9013477550946897860
destiny <desphixs@gmail.com>
2026-05-08 16:02:30 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-indigo-500 text-4xl font-bold text-center w-full h-screen flex items-center justify-center">Project</div>
+  )
+}
+
+export default App
